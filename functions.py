f06f625560e09be100fcbf5e5ac58ecbc465a87a
franamr <106500914+franamr@users.noreply.github.com>
2023-12-17 21:24:19 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,78 +1,88 @@
 
 import numpy as np
 
-def ACA(M, tol = 1e-8, k = 40):
+
+def aca(matrix, tolerance = 1e-8, max_iter = 40):
     '''
     Adaptive Cross Approximation
     
-    Input: M matrix, tol stoping tolerance, k max iterations
-    Output: A, B rectangular matrices, M = A*B
+    Input 
+    matrix: matrix, 
+    tolerance: stoping tolerance, 1e-8 by default
+    max_iter: maximum number of iterations, 40 by default
+    
+    Output: 
+    A, B rectangular matrices, M = A.T*B
     '''
-    n = np.shape(M)[0]
-    #R = np.zeros((t, s))
+    n = np.shape(matrix)[0]
     rows = []
     cols = []
 
-    for v in range(1, k+1):
-        # Compute the maximal entry in modulus
-        i_max, j_max = np.unravel_index(np.abs(M).argmax(), M.shape)
-        delta = M[i_max, j_max]
+    for v in range(1, max_iter+1):
+        # Compute the maximal entry
+        i_max, j_max = np.unravel_index(np.abs(matrix).argmax(), matrix.shape)
+        delta = matrix[i_max, j_max]
 
         if delta == 0:
-            
             return np.array(rows),np.array(cols)
+        
         else:
-
-            a_row = M[:, j_max].copy()
+            a_row = matrix[:, j_max].copy()
             rows.append(a_row)
-            b_col = M[i_max, :].copy() / delta
+            b_col = matrix[i_max, :].copy()/delta
             cols.append(b_col)
-            M -= np.outer(a_row, b_col)
+            matrix -= np.outer(a_row, b_col)
             eps = (np.linalg.norm(a_row)*np.linalg.norm(b_col))
-            if eps < tol:
+            if eps < tolerance:
                 return np.array(rows),np.array(cols)
 
     return np.array(rows), np.array(cols)
   
-def aca_matvec(M,b,k,tol = 1e-8):
+  
+def aca_matvec(matrix, vector, max_iter, tolerance = 1e-8):
     '''
-    Matvec operation for matriz with Adaptive Cross Approximation
+    Matvec operation for matrix with Adaptive Cross Approximation
     
-    Input: 
-    M : Matrix 
-    b: vector
+    Input
+    matrix : Matrix 
+    vector: vector
+    max_iter: maximum number of iterations
+    tolerance: ACA tolerance, 1e-8 by default
     
     Output:
-    a = Mb
+    vector: matrix*vector
     '''
-    rows, cols = ACA(M, tol,k)
-    return rows.T@(cols@b)
+    rows, cols = aca(matrix, tolerance, max_iter)
+    return rows.T@(cols@vector)
 
-def hmatvec(M,b, max_p):
+
+def hmatvec(matrix, vector, max_dimension):
     '''
-    Matvec con hierarchical matrices
+    Matvec operation for matrix with hierarchical compression
+    with weak admissibility condition.
     
     Input: 
-    M: Matrix
-    b: vecotr
+    matrix: Matrix
+    vector: vecotr
+    max_dimension: dimension of dense sub-matrix block
     
     Output:
-    a = Mb
+    vector: Matrix*vector
     '''
-    n = np.shape(b)[0]
-    a = np.zeros(n)
+    n = np.shape(vector)[0]
     
-    if n <= max_p:
-        return np.dot(M,b)
+    if n <= max_dimension:
+        return np.dot(matrix, vector)
     else:
-        
-        M11 = M[:n//2, :n//2]
-        M12 = M[:n//2, n//2:]
-        M21 = M[n//2:, :n//2]
-        M22 = M[n//2:, n//2:]
-        b1 = b[:n//2]
-        b2 = b[n//2:]
-        result1 = hmatvec(M11, b1, max_p) + aca_matvec(M12, b2, np.shape(M12)[0])
-        result2 = hmatvec(M21, b1, max_p) + aca_matvec(M22, b2, np.shape(M22)[0])
+        matrix11 = matrix[:n//2, :n//2]
+        matrix12 = matrix[:n//2, n//2:]
+        matrix21 = matrix[n//2:, :n//2]
+        matrix22 = matrix[n//2:, n//2:]
+        vector1 = vector[:n//2]
+        vector2 = vector[n//2:]
+        result1 = hmatvec(matrix11, vector1, max_dimension) + \
+            aca_matvec(matrix12, vector2, np.shape(matrix12)[0])
+        result2 = hmatvec(matrix21, vector1, max_dimension) + \
+            aca_matvec(matrix22, vector2, np.shape(matrix22)[0])
 
         return np.concatenate((result1,result2))
